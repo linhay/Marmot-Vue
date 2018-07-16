e6573997ee2a0070dc52dec43378c3cd1615aaa6
linhey <158179948@qq.com>
2018-07-16 21:54:50 +0800

    文件迁移

---
+++ src/components/children/location.vue
@@ -0,0 +1,47 @@
+<template>
+<div>
+    <div class="title">{{name}}</div>
+    <el-collapse v-for="(item, index) in items" :key="item.index" accordion>
+
+        <el-collapse-item class="subTitle" :title=item.title>
+            <div class="block" v-if="item.slider">
+                <span class="demonstration">默认</span>
+                <el-slider v-model=item.body.value :min=item.slider.min :max=item.slider.max :step=item.slider.step show-input=true></el-slider>
+            </div>
+
+            <el-button class="btn" type="success" size="medium" v-on:click="event(index)">点击运行</el-button>
+            <div class="result-tips">返回示例:</div>
+            <div class="result">{{item.result}}</div>
+        </el-collapse-item>
+
+    </el-collapse>
+</div>
+</template>
+
+<script>
+export default {
+    name: "location",
+    data() {
+        return {
+            name: "location",
+            items: [{
+                title: "location.fetch",
+                event: "sp://location/fetch",
+                result: ""
+            }]
+        }
+    },
+
+    methods: {
+        event: function (index) {
+            JSBridge(this.items[index].event, this.items[index].body)
+                .then(result => {
+                    this.items[index].result = result;
+                })
+                .catch(err => {
+                    this.items[index] = err;
+                });
+        }
+    }
+};
+</script>
